ac76f5dace2057bcac889c55514cf72807cb9090
Big Patto <apatto@hotmail.com>
2014-10-27 16:28:13 +1100

    Now does standard deviation

--- peak_switching.R
+++ peak_switching.R
@@ -30,9 +30,6 @@ poly_A_peak_puller<- function(peak,bam_file, gff){
         goi<- output
         colnames(goi) <- c('chr', 'program', 'type', 'peak start', 'peak end','DNS','orientation', 'DNS2','information','numbers')
         
-        # Print peaks used for reference
-        cat('peaks used: \n')
-        print(goi)
         
         # Split the gene of interest by orientation
         split_goi <- split(goi, goi[,'orientation'],drop = TRUE)
@@ -62,8 +59,7 @@ poly_A_peak_puller<- function(peak,bam_file, gff){
                         
                         reads_in_peak_neg <- length (no_of_as1)
                         list_of_peaks <-  c(list_of_peaks, length(no_of_as1))
-                        print (c('number of reverse strand reads per peak', line))
-                        print (reads_in_peak_neg)
+                        
                         
                         
                         # Add succesive peaks together
@@ -111,8 +107,7 @@ poly_A_peak_puller<- function(peak,bam_file, gff){
                                 
                                 reads_in_peak_pos <- length (no_of_as2)
                                 
-                                print (c('number of forward strand reads per peak',line)) 
-                                print (reads_in_peak_pos)
+                                
                                 
                                 # Print to output file for later identification of 3' UTR switching
                                 p <- c(line,reads_in_peak_pos)
@@ -128,20 +123,23 @@ poly_A_peak_puller<- function(peak,bam_file, gff){
         all_poly_a_tails<- sort(c(all_poly_a_tails_plus, all_poly_a_tails_minus))
         
         
-        cat('next condition\n')
+        
         
         
         return (all_poly_a_tails)
         
 }
 
-peak_switching<- function(peak_list,bam_list, gff, number_of_replicates = 3){
+peak_switching<- function(peak_list,bam_list, gff, number_of_replicates = 3, number_of_conditions =2){
+        #This function currently only works for two conditions
         
+        # Read in the gff file
         gff_file <- read.table (gff, sep = "\t", header = FALSE)
         
-        
+        # Set an empty read couts list for later use
         read_counts<- list()
-       
+        
+        # Get differnt peaks for each BAM file and store them in read counts
         for (bam_file in bam_list){
                 read_count_condition1 <- lapply (peak_list[[1]], poly_A_peak_puller, bam_file,gff_file)
                 
@@ -154,42 +152,79 @@ peak_switching<- function(peak_list,bam_list, gff, number_of_replicates = 3){
                 read_counts <- c(read_counts, read_count_condition2)
                 
         }
+        # Get the lengths of the poly A tail lists stored in read counts to get read counts
+        length_list <- lapply (read_counts, length)
+        # Put these values into a data frame
+        length_frame <- do.call('rbind', length_list)
+        # Add the list of file names at their corresponding counts 
+        length_frame <- cbind(paste(bam_list), as.numeric(length_frame))
+        
+        # Order the data frame by name so that like conditions match up (may mave condition out of input order)
+        length_frame <- length_frame[order(length_frame[,1], decreasing = FALSE),]
         
         
-       
+        # Get totals of peaks per conition
+        totals <- colSums(matrix(as.numeric(length_frame[,2]), nrow=length(peak_list)))
         
         
-                
-                length_list <- lapply (read_counts, length)
-                length_frame <- do.call('rbind', length_list) 
-                length_frame <- cbind(paste(bam_list), as.numeric(length_frame))
-                
-                
-            
-                length_frame <- colMeans(matrix(as.numeric(length_frame[,2]), nrow=length(bam_list)/2))
-                
-                length_frame <- as.data.frame(length_frame)
-                names <- paste(bam_list)[seq(1, length(paste(bam_list)), number_of_replicates)]
-                length_frame <- cbind(names,length_frame)
-                length_frame <- length_frame[order(length_frame$names),]
-                
-                totals <- colSums(matrix(as.numeric(length_frame[,2]), nrow=length(peak_list)))
-                totals <- rep(totals, each = length(peak_list)) 
-                print(totals)
-                length_frame <- cbind(length_frame, totals)
-                length_frame[,2] <- length_frame[['length_frame']] / (length_frame[['totals']])*100
-                
-                print(length_frame)
-               
-                
-                
+        # Put totals in cols next to the vales that were added to reach them
+        totals <- rep(totals, each = length(peak_list)) 
         
-        par(mar=c(4,4,4,4))
-        barplot(beside=TRUE, xlim=c(0,15), ylim = c(0,100), length_frame[,2], xaxt= 'n' ,col=c('green','purple'),space =c(0,0), ylab = 'proportion peak used', xlab= '', width = 3:3, names.arg=c(paste(bam_list[[1]]),paste(bam_list[[1]]),paste(bam_list[[2]]),paste(bam_list[[2]])))
+        # Calculate percentage peaks used
+        length_frame <- cbind(length_frame, totals)
+        
+        length_frame[,2] <- as.numeric(length_frame[,2]) / (as.numeric(length_frame[,3]))*100
+        
+        # Split up your data frames into peak groups
+        length_frame1 <- length_frame[,-3] [seq(1, nrow(length_frame), 2),]
+        length_frame2 <- length_frame[,-3]  [seq(2, nrow(length_frame), 2),]
+        
+        stdev_lists1 <- split(matrix(as.numeric(length_frame1[,2])), ceiling(seq_along(length_frame1[,2])/number_of_replicates))
+        print(stdev_lists1)
+        stdev_lists1 <-lapply(stdev_lists1,sd)
+        stdev_lists2 <- split(matrix(as.numeric(length_frame2[,2])), ceiling(seq_along(length_frame2[,2])/number_of_replicates))
+        
+        stdev_lists2 <-lapply(stdev_lists2,sd)
+        print(stdev_lists2)
+        
+        
+        sdevs<- as.numeric(c(stdev_lists1, stdev_lists2))
+        sdevs <-cbind(paste(c(rep(bam_list[1],rep=2),rep(bam_list[(length(bam_list)/number_of_conditions)+1],rep=2))),sdevs)
+        print(sdevs)
+        sdevs <-sdevs [order(sdevs[,1], decreasing = TRUE),]
+        print(sdevs)
+        # Get the avearge proportion of these groups
+        length_frame1 <- colMeans(matrix(as.numeric(length_frame1[,2]), nrow=length(bam_list)/number_of_conditions))
+        length_frame2 <- colMeans(matrix(as.numeric(length_frame2[,2]), nrow=length(bam_list)/number_of_conditions))
+        
+        
+        length_frame1 <- cbind(length_frame1)
+        length_frame2 <- cbind(length_frame2)
+        
+        # Put them back together
+        length_frame<- rbind(length_frame1,length_frame2)
+        
+        # Add names back in
+        length_frame<-cbind(paste(c(rep(bam_list[1],rep=2),rep(bam_list[(length(bam_list)/number_of_conditions)+1],rep=2))),length_frame)
+        
+        # Order by sample
+        length_frame <- length_frame[order(length_frame[,1], decreasing = TRUE),]
+        
+        avgs <- as.numeric(length_frame[,2]) 
+        points<- c(1.5,4.5,7.5,10.5)
+        
+        
+        barplot(main= 'peak switching', beside=TRUE, xlim=c(0,15), ylim = c(0,100), as.numeric(length_frame[,2]), xaxt= 'n' ,col=c('green','purple'),space =c(0,0), ylab = 'percentage peak used', axes=FALSE, xlab= '', width = 3:3, names= length_frame[,1])
+        
+        
         legend("topright", legend = paste('peak',peak_list), fill = c("green","purple"),text.width=5, bty = 'n')
-        axis(1,cex.axis=0.3,las=3, at=c(1.5,4.5,7.5,10.5), labels = paste(length_frame[,1]))
+        
+        
+        axis(2, pos=0, at= c(0,25,50,75,100), tick = 25)
+        
+        
+        
+        axis(1,hadj= 0.85,cex.axis=0.2,las=3, at=c(1.5,4.5,7.5,10.5), labels = length_frame[,1] )
+        arrows(x0 = points, y0 = avgs, x1= points, y1 = avgs + as.numeric(sdevs[,2]), length=0.05, angle=90, code=3 )
         
 }
-
-
-
